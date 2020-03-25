4b92256eae470ab9807359cf9d8d610c92e147ab
Alexander Chernykh <forc1@yandex.ru>
2020-03-25 19:22:31 +0300

    Сделал экран авторизации

--- VKapp/VKapp/LoginViewController.swift
+++ VKapp/VKapp/LoginViewController.swift
@@ -9,12 +9,57 @@
 import UIKit
 
 class LoginViewController: UIViewController {
-
+    @IBOutlet weak var scrollViewBottomConstraint: NSLayoutConstraint!
+    
+    @IBOutlet weak var weatherLabel: UILabel!
+    @IBOutlet weak var loginLabel: UILabel!
+    @IBOutlet weak var passwordLabel: UILabel!
+    @IBOutlet weak var loginTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    
+    @IBOutlet weak var loginButton: UIButton!
+    
+    @IBOutlet weak var scrollView: UIScrollView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-      
+        weatherLabel.text = "VK"
+        
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWasShown(notification:)),
+            name: UIResponder.keyboardWillShowNotification,
+            object: nil)
+        NotificationCenter.default.addObserver(
+            self,
+            selector: #selector(keyboardWillBeHidden(notification:)),
+            name: UIResponder.keyboardWillHideNotification,
+            object: nil)
     }
 
-
+    @IBAction func loginPressed() {
+        let login = loginTextField.text!
+        let password = passwordTextField.text!
+        
+        
+        if login == "admin" && password == "123456" {
+            print("успешная авторизация")
+        } else {
+            print("неуспешная авторизация")
+        }
+    }
+    
+    @objc func keyboardWasShown(notification: Notification) {
+        let userInfo = (notification as NSNotification).userInfo as! [String: Any]
+        let frame = userInfo[UIResponder.keyboardFrameEndUserInfoKey] as! CGRect
+        
+        scrollViewBottomConstraint.constant = frame.height
+//        scrollView.contentOffset = CGPoint(x: 0, y: frame.height)
+    }
+    
+    @objc func keyboardWillBeHidden(notification: Notification) {
+        scrollViewBottomConstraint.constant = 0
+    }
+    
 }
 
