00eb2dbda3f8bbdf7c741e93d5cef00b910fb1ff
Alexander Chernykh <forc1@yandex.ru>
2020-04-05 17:42:47 +0300

    добавил фото в коллекцию

--- VKapp/VKapp/Model/ImagesCollectionViewController.swift
+++ VKapp/VKapp/Model/ImagesCollectionViewController.swift
@@ -27,7 +27,7 @@ class ImagesCollectionViewController: UICollectionViewController {
 
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
 
-        return 1
+        return friendsImages.count
     }
 
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
